26258ec1d0d4c72eeceddf1c3c7d80303e706633
Myrio SOPHIE <myriosophie@macbookpro.home>
2026-06-14 14:27:07 +0200

    fix: revenir au domaine canonique myracoustic.com (sans www)

--- app/robots.js
+++ app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.myracoustic.com/sitemap.xml',
+    sitemap: 'https://myracoustic.com/sitemap.xml',
   };
 }
